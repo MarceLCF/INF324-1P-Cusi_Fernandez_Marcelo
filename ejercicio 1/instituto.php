8d044f35ca775796c0c63d6c861c40b462433416
MarceLCF <70829087+MarceLCF@users.noreply.github.com>
2023-10-09 16:39:53 -0400

    Add files via upload

---
+++ ejercicio 1/instituto.php
@@ -0,0 +1,15 @@
+<?php 
+include("header.php");
+?>
+<div class="container-fluid" style="border-left:25px solid #5283dE;">
+    <section id="instituto">
+        <section id="fondo">
+            <img src="imagenes/info.jpg" />
+        </section>
+
+    </section>
+</div>
+
+<?php
+    include("footer.php");
+?>
