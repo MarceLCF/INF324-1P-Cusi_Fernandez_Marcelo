47e43c3c81a9a1c2f66978032c27dbd7441acc93
MarceLCF <70829087+MarceLCF@users.noreply.github.com>
2023-10-09 17:07:00 -0400

    Delete ejercicio 2 directory

--- ejercicio 2/cambio.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    //$usuario = $_REQUEST['usuario'];
-    $col = $_POST['colorInput'];
-    $_SESSION['color'] = $col;
-    //echo $_SESSION["color"];
-    header("Location: index.php");
-?>
--- ejercicio 2/footer.php
+++
@@ -1,7 +0,0 @@
-    <hr>
-    <footer style="background-color: <?php echo $color;?>">
-        <p>&copy; <?php echo date("d-m-Y"); ?>, INF 324</p>
-        <b>Carrera de Informatica - UMSA</b>
-    </footer>
-</body>
-</html>
